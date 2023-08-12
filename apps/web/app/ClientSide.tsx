bfb3c80913f7d4d68feda64e017e8959b3b2633a
Kaleb Lim <kaleb@pickupp.io>
2023-08-12 18:56:37 +0700

    fix trpc response

--- apps/web/app/ClientSide.tsx
+++ apps/web/app/ClientSide.tsx
@@ -6,9 +6,7 @@ import { useEffect, useState } from 'react';
 export default function ClientSide() {
   const [greeting, setGreeting] = useState('');
   useEffect(() => {
-    trpc.hello
-      .query({})
-      .then((response) => setGreeting(response));
-  }, []);
-  return <p>I am client side: {greeting}</p>;
+    trpc.hello.query({}).then((response) => setGreeting(response));
+  });
+  return <p>I am client side new: {greeting}</p>;
 }
