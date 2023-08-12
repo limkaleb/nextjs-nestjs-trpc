09a73b5f7401b5aa3a4e2794429b43d27023a5fd
Kaleb Lim <kaleb@pickupp.io>
2023-08-12 19:11:12 +0700

    fix trpc response

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,11 +2,10 @@ import { trpc } from '@web/app/trpc';
 // import ClientSide from './ClientSide';
 
 export default async function Home() {
-  const response = await trpc.hello.query({ name: 'kaleb' });
-  console.log('responsee: ', response)
+  // const response = await trpc.hello.query({ name: 'kaleb' });
   return (
     <div>
-      <p>server side - {response}</p>
+      <p>server side</p>
       {/* <ClientSide /> */}
     </div>
   );
